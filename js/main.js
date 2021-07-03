7f5180c28c4a7859232637c8f2cdef3efda798d6
Fatumxxx <vasylzaya@gmail.com>
2021-07-03 15:43:51 +0300

    added project teleko

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(function () {
+    $('.main__items').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        fade: true,
+    });
+
+
+});
